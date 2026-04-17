845c857802d52cfa377b28a50ed994c19f5acad3
LeviSolomon270 <solomonlevi270@gmail.com>
2026-04-17 16:37:42 +0200

    constructure

--- app/src/main/java/com/example/musicapp/song.kt
+++ app/src/main/java/com/example/musicapp/song.kt
@@ -2,12 +2,24 @@ package com.example.musicapp
 
 import android.util.Log
 
-class song {
+class song (songName : String, songartist : String, songyear : Int, songduration : String){
 
     var name : String =""
     var artist : String=""
-    var year : Int =()
+    var year : Int = 0
     var duration : String =""
+ constructor(songName : String, songartist : this( : this()songName, songartist,0,")
+    String, songyear : Int, songduration : String songfavourite : Boolean)
+
+    // Constructor
+    init{
+        name = songName
+        artist = songartist
+        year = songyear
+        duration = songduration
+    }
+
+
 
     fun Play(){
     Log.v("Song", "$name Playing now")
