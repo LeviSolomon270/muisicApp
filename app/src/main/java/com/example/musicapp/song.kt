4fc2ed572b73797da31f4a9f494286adf40477a9
LeviSolomon270 <solomonlevi270@gmail.com>
2026-04-17 16:03:09 +0200

    wrote the code

---
+++ app/src/main/java/com/example/musicapp/song.kt
@@ -0,0 +1,36 @@
+package com.example.musicapp
+
+import android.util.Log
+
+class song {
+
+    var name : String =""
+    var artist : String=""
+    var year : Int =()
+    var duration : String =""
+
+    fun Play(){
+    Log.v("Song", "$name Playing now")
+
+    }
+
+    fun Pause(){
+        Log.v("Song", "$name Playing now")
+
+        fun Stop(){
+            Log.v("Song", "$name Playing now")
+        }
+
+        fun Favourite():Boolean{
+            Log.v("Song", "$name Playing now")
+            return true
+        }
+
+
+
+
+    }
+
+
+
+}
